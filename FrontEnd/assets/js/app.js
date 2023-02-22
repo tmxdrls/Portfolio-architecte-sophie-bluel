6ba1857359049c4bc0ca898a5756d0a01ed5c684
Mathieu Dourlens <matdourlens95@gmail.com>
2023-02-22 18:07:59 +0100

    création des boutons filtres et ajout d'event

--- FrontEnd/assets/js/app.js
+++ FrontEnd/assets/js/app.js
@@ -7,16 +7,22 @@ fetch("http://localhost:5678/api/categories")
 
 .then(category=> {
     console.log(category);
-    const ul= document.createElement("ul")
-    const tous= document.createElement("li")
-    tous.innerHTML= "Tous"
-    ul.appendChild(tous)
-    gallery.appendChild(ul)
+    const menu= document.createElement("menu")
+    const liAll= document.createElement("li")
+    menu.appendChild(liAll)
+    const buttonAll= document.createElement("button")
+    liAll.appendChild(buttonAll)
+    buttonAll.textContent= "Tous"
+    gallery.appendChild(menu)
+    buttonAll.addEventListener('click',function{
+        console.log(category.id)
+    })
     for(let filter of category){
-        const li= document.createElement("li")
-        
-        console.log(filter.name);
-        
+        const liFilter= document.createElement("li")
+        const buttonFilter= document.createElement("button")
+        liFilter.appendChild(buttonFilter)
+        menu.appendChild(liFilter)
+        buttonFilter.textContent= filter.name
     }
 
 })
@@ -26,7 +32,6 @@ fetch("http://localhost:5678/api/works")
 .then(response=> response.json())
 
 .then(works=> {
-    console.log(works);
     for(let project of works){
         console.log(project);
         const figure= document.createElement("figure")
@@ -36,37 +41,8 @@ fetch("http://localhost:5678/api/works")
         figure.appendChild(image)
         const figcaption= document.createElement("figcaption")
         figcaption.textContent= project.title
-        /*figure.appendChild(image,figcaption)*/
         figure.appendChild(figcaption)
         const figureHtml= document.querySelector(".gallery figure")
         gallery.appendChild(figure)
-    };
+    }
 })
-
-
-
-    /*
-let Filter= {
-all: works.length,
-object: works[0, 4],
-appartment: works[1 ,5 ,8],
-hotelEtRestaurant: works[2 ,9 ,10],
-};
-console.log(Filter);
-
-const monSetAll= new Set();
-
-monSetAll.add(works.length);
-
-const monSetObject= new Set();
-
-monSetObject.add(works[0 ,4]);
-
-const monSetAppartment= new Set();
-
-monSetAppartment.add(works[1 ,5 ,8]);
-
-const monSetHotelEtRestaurant= new Set();
-
-monSetHotelEtRestaurant.add(works[2 ,9 ,10]);
-*/
